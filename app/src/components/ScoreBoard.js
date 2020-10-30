c841d5c8e0695d25680de2a8b29c36f892997059
Leon's BigMac <liang268@purdue.edu>
2020-10-30 12:17:07 -0500

    improve scoreboard UI

--- app/src/components/ScoreBoard.js
+++ app/src/components/ScoreBoard.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
-function ScoreBoard({ result, setState}) {
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+    },
+  }));
+
+function ScoreBoard({ score, setState}) {
+    console.log(score)
+    const classes = useStyles();
+    // console.log(localStorage.getItem('totalScore'))
     return (
-        <div>
-            <p>Your score here</p>
-            <button type="button" className="button" onClick={setState}>
-                Try Again
-            </button>
+        <div className={classes.root}>
+            <Grid container
+                    spacing={3}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minHeight: '100vh' }}>
+                <Grid item xs={12}>
+                    <Card>
+                        <Paper className={classes.paper}>Your score here</Paper>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
+                <Button onClick={setState} variant="contained">
+                        Try Again
+                </Button>
+            </Grid>
+            </Grid>
         </div>
         
     )
